6a1088df10ec22ef601f4f18c3e9894934ac0381
uzair-2006 <dodkutta@gmail.com>
2024-01-21 00:36:40 +0500

    fixed error

--- app/(site)/register/page.jsx
+++ app/(site)/register/page.jsx
@@ -1,6 +1,6 @@
 import RegisterForm from '@/app/components/RegisterForm';
 import { getServerSession } from 'next-auth';
-import { authOptions } from '../api/auth/[...nextauth]/route';
+import { authOptions } from '@/app/api/auth/[...nextauth]/route';
 import { redirect } from 'next/navigation';
 const Page = async () => {
     const session = await getServerSession(authOptions)
