c72d756361c7f943cba388dfb5918c0127cf699d
uzair-2006 <dodkutta@gmail.com>
2024-01-21 12:11:02 +0500

    fixed

--- app/components/RegisterForm.jsx
+++ app/components/RegisterForm.jsx
@@ -40,7 +40,6 @@ const RegisterForm = () => {
                             headers: {
                                 'Accept': 'application/json',
                                 'Content-Type': 'application/json',
-                                'Access-Control-Allow-Origin': 'https://employee-management-green.vercel.app',
                             },
                             method: "POST",
                             body: JSON.stringify(data)
