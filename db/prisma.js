4a7a36a919d1d14b4a5699dd72631b2bb54571d9
uzair-2006 <dodkutta@gmail.com>
2024-01-21 11:29:24 +0500

    df

--- db/prisma.js
+++ db/prisma.js
@@ -2,11 +2,12 @@
 
 import { PrismaClient } from '@prisma/client';
 
-let prisma;
+let prisma = PrismaClient;
 
 if (process.env.NODE_ENV === 'production') {
     prisma = new PrismaClient();
 } else {
+    // In development, use a global variable to avoid reconnecting on each Hot Module Replacement (HMR) update
     if (!global.prisma) {
         global.prisma = new PrismaClient();
     }
